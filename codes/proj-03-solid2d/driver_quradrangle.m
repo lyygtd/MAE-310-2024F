28defb64534b2360a2509a17e877fc6460472d56
lyygtd <532927396@qq.com>
2025-01-06 15:22:58 +0800

    完成四边形网格的纽曼边界条件

--- codes/proj-03-solid2d/driver_quradrangle.m
+++ codes/proj-03-solid2d/driver_quradrangle.m
@@ -7,15 +7,19 @@ kappa = 1.0; % conductivity
 % exact = @(x,y) x*(1-x)*y*(1-y);
 % exact_x = @(x,y) (1-2*x)*y*(1-y);
 % exact_y = @(x,y) x*(1-x)*(1-2*y);
-% 
-f = @(x,y) 2.0*kappa*x*(1-x) + 2.0*kappa*y*(1-y); % source term
-
+%
+% f = @(x,y) 2.0*kappa*x*(1-x) + 2.0*kappa*y*(1-y); % source term
+f = @(x,y) 0;
 % quadrature rule
 n_int_xi  = 3;
 n_int_eta = 3;
 n_int     = n_int_xi * n_int_eta;
-    [xi, eta, weight] = Gauss2D(n_int_xi, n_int_eta);
+[xi, eta, weight] = Gauss2D(n_int_xi, n_int_eta);
 %     ξ   η
+
+% quadrature rule for h boundary
+n_int_h = 10;
+[xi_h, weight_h] = Gauss(n_int_h, -1, 1);
 
 % mesh数据导入
 fileID = fopen("..\..\gmsh-files\quarter-plate-with-hole-quad.msh", 'r');
@@ -25,9 +29,9 @@ end
 
 x_coor = [];
 y_coor = [];
-maxCols = 0; 
+maxCols = 0;
 inDataSection = false;
- 
+
 tline = fgetl(fileID);
 
 %生成x_coor和y_coor
@@ -107,29 +111,49 @@ n_np   = length(x_coor); % total number of nodal points
 
 gg = zeros(n_np,1); % g boundary condition
 hh = zeros(n_np,1); % h boundaty condition
-
+left_h_boundary_nodes = [];
+right_h_boundary_nodes = [];
+bottom_h_boundary_nodes = [];
+top_h_boundary_nodes = [];
+circle_h_boundary_nodes = [];
 % ID array
 ID = zeros(n_np,1);
 counter = 0;
 
 for nn = 1 : n_np
-    if x_coor(nn) == -1 % left boundary
+    if x_coor(nn) == -1  && y_coor(nn) ~= -0.5 % left boundary
+        % counter = counter +1;
+        % ID(nn) = counter;
+        % left_h_boundary_nodes = [left_h_boundary_nodes; nn];
+        % h_left = @(y) 40;
+        ID(nn) = 0;
+        gg(nn) = 1;
+    
+    elseif x_coor(nn) == 1 &&  y_coor(nn) ~= 1 && y_coor(nn) ~= -1 % right boundary
         counter = counter +1;
         ID(nn) = counter;
-        hh(nn) = 40;
-    elseif y_coor(nn) == -1 % bottom boundary
+        right_h_boundary_nodes = [right_h_boundary_nodes; nn];
+        h_right = @(y) 1;
+
+    elseif y_coor(nn) == 1 &&  x_coor(nn) ~= -1 % top boundary
         counter = counter +1;
         ID(nn) = counter;
-        hh(nn) = 40;
-    elseif x_coor(nn) == 1 % right boundary
-        ID(nn) = 0;
-        gg(nn) = 0;
-    elseif y_coor(nn) == 1 % top boundary
-        ID(nn) = 0;
-        gg(nn) = 0;
+        top_h_boundary_nodes = [top_h_boundary_nodes; nn];
+        h_top = @(x) 0;
+        % ID(nn) = 0;
+        % gg(nn) = 1;
+
+    elseif y_coor(nn) == -1 && x_coor(nn) ~= -0.5 % bottom boundary
+        counter = counter +1;
+        ID(nn) = counter;
+        bottom_h_boundary_nodes = [bottom_h_boundary_nodes; nn];
+        h_bottom = @(x) 0;
+
     elseif abs(sqrt((x_coor(nn)+1)^2 + (y_coor(nn)+1)^2) - 0.5) <= 1e-12 % circle boundary
-        ID(nn) = 0;
-        gg(nn) = 1;   % 设置为g边界
+       counter = counter +1;  %请永远设为h=0的边界
+        ID(nn) = counter;
+        h_circle = @(x,y) 0;
+        
     else
         counter = counter +1;
         ID(nn) = counter;
@@ -147,69 +171,128 @@ F = zeros(n_eq, 1);
 
 % loop over element to assembly the matrix and vector
 for ee = 1 : n_el
-  x_ele = x_coor( IEN(ee, 1:n_en) );    % 局部节点的全局横坐标 = x_ele(a)
-  y_ele = y_coor( IEN(ee, 1:n_en) );    % 局部节点的全局纵坐标 = y_ele(a)
-  
-  k_ele = zeros(n_en, n_en); % element stiffness matrix
-  f_ele = zeros(n_en, 1);    % element load vector
-  g_ele = zeros(n_en, 1);    % element g boundary contition
+    x_ele = x_coor( IEN(ee, 1:n_en) );    % 局部节点的全局横坐标 = x_ele(a)
+    y_ele = y_coor( IEN(ee, 1:n_en) );    % 局部节点的全局纵坐标 = y_ele(a)
 
-  for ll = 1 : n_int
-    x_l = 0.0; y_l = 0.0;
-    dx_dxi = 0.0; dx_deta = 0.0;
-    dy_dxi = 0.0; dy_deta = 0.0;
-    for aa = 1 : n_en
-      x_l = x_l + x_ele(aa) * Quad(aa, xi(ll), eta(ll));  %坐标变换Mapping
-      y_l = y_l + y_ele(aa) * Quad(aa, xi(ll), eta(ll));    
-      [Na_xi, Na_eta] = Quad_grad(aa, xi(ll), eta(ll));
-      dx_dxi  = dx_dxi  + x_ele(aa) * Na_xi;
-      dx_deta = dx_deta + x_ele(aa) * Na_eta;
-      dy_dxi  = dy_dxi  + y_ele(aa) * Na_xi;
-      dy_deta = dy_deta + y_ele(aa) * Na_eta;
-    end
-    
-    detJ = dx_dxi * dy_deta - dx_deta * dy_dxi;
-    
-    for aa = 1 : n_en
-      Na = Quad(aa, xi(ll), eta(ll));
-      [Na_xi, Na_eta] = Quad_grad(aa, xi(ll), eta(ll));
-      Na_x = (Na_xi * dy_deta - Na_eta * dy_dxi) / detJ;
-      Na_y = (-Na_xi * dx_deta + Na_eta * dx_dxi) / detJ;
-      
-      f_ele(aa) = f_ele(aa) + weight(ll) * detJ * f(x_l, y_l) * Na;
-      %h_ele(aa) = h_ele(aa) + weight(ll) * detJ * hh(IEN(ee,aa)) * Na;
-      for bb = 1 : n_en
-        Nb = Quad(bb, xi(ll), eta(ll));
-        [Nb_xi, Nb_eta] = Quad_grad(bb, xi(ll), eta(ll));
-        Nb_x = (Nb_xi * dy_deta - Nb_eta * dy_dxi) / detJ;
-        Nb_y = (-Nb_xi * dx_deta + Nb_eta * dx_dxi) / detJ;
-        
-        k_ele(aa, bb) = k_ele(aa,bb) + weight(ll) * detJ * kappa * (Na_x * Nb_x + Na_y * Nb_y);
-        if LM(ee, bb) == 0
-            g_ele(aa) = g_ele(aa) + weight(ll) * detJ * kappa * (Na_x * Nb_x + Na_y * Nb_y) * gg(IEN(ee,bb));
+    k_ele = zeros(n_en, n_en); % element stiffness matrix
+    f_ele = zeros(n_en, 1);    % element load vector
+    g_ele = zeros(n_en, 1);    % element g boundary contition
+    h_ele = zeros(n_en, 1);
+
+    for ll = 1 : n_int
+        x_l = 0.0; y_l = 0.0;
+        dx_dxi = 0.0; dx_deta = 0.0;
+        dy_dxi = 0.0; dy_deta = 0.0;
+        for aa = 1 : n_en
+            x_l = x_l + x_ele(aa) * Quad(aa, xi(ll), eta(ll));  %坐标变换Mapping
+            y_l = y_l + y_ele(aa) * Quad(aa, xi(ll), eta(ll));
+            [Na_xi, Na_eta] = Quad_grad(aa, xi(ll), eta(ll));
+            dx_dxi  = dx_dxi  + x_ele(aa) * Na_xi;
+            dx_deta = dx_deta + x_ele(aa) * Na_eta;
+            dy_dxi  = dy_dxi  + y_ele(aa) * Na_xi;
+            dy_deta = dy_deta + y_ele(aa) * Na_eta;
         end
 
-      end % end of bb loop
-    end % end of aa loop
-  end % end of quadrature loop
+        detJ = dx_dxi * dy_deta - dx_deta * dy_dxi;
 
-  for aa = 1 : n_en
-    PP = LM(ee, aa);
-    if PP > 0
-      F(PP) = F(PP) + f_ele(aa) - g_ele(aa);
-      
-      for bb = 1 : n_en
-        QQ = LM(ee, bb);
-        if QQ > 0
-          K(PP, QQ) = K(PP, QQ) + k_ele(aa, bb);
-        else
-          % modify F with the boundary data
-          % here we do nothing because the boundary data g is zero or
-          % homogeneous    
+        for aa = 1 : n_en
+            Na = Quad(aa, xi(ll), eta(ll));
+            [Na_xi, Na_eta] = Quad_grad(aa, xi(ll), eta(ll));
+            Na_x = (Na_xi * dy_deta - Na_eta * dy_dxi) / detJ;
+            Na_y = (-Na_xi * dx_deta + Na_eta * dx_dxi) / detJ;
+
+            f_ele(aa) = f_ele(aa) + weight(ll) * detJ * f(x_l, y_l) * Na;
+            for bb = 1 : n_en
+                Nb = Quad(bb, xi(ll), eta(ll));
+                [Nb_xi, Nb_eta] = Quad_grad(bb, xi(ll), eta(ll));
+                Nb_x = (Nb_xi * dy_deta - Nb_eta * dy_dxi) / detJ;
+                Nb_y = (-Nb_xi * dx_deta + Nb_eta * dx_dxi) / detJ;
+
+                k_ele(aa, bb) = k_ele(aa,bb) + weight(ll) * detJ * kappa * (Na_x * Nb_x + Na_y * Nb_y);
+                if LM(ee, bb) == 0
+                    g_ele(aa) = g_ele(aa) + weight(ll) * detJ * kappa * (Na_x * Nb_x + Na_y * Nb_y) * gg(IEN(ee,bb));
+                end
+
+            end % end of bb loop
+        end % end of aa loop
+    end % end of quadrature loop
+
+    
+    for qua = 1 : n_int_h
+        dx_dxi_h = 0.0;
+        x_l_h = 0.0;
+        left_node_conter = 0;
+        right_node_conter = 0;
+        top_node_conter = 0;
+        bottom_node_conter = 0;
+        for aa = 1 : n_en
+
+            if ismember(IEN(ee,aa),left_h_boundary_nodes)
+                left_node_conter = left_node_conter + 1;
+                x_l_h = x_l_h + y_ele(aa) * PolyShape(1, left_node_conter, xi_h(qua), 0);
+                dx_dxi_h = dx_dxi_h + y_ele(aa) * PolyShape(1, left_node_conter, xi_h(qua), 1);
+            elseif ismember(IEN(ee,aa),right_h_boundary_nodes)
+                right_node_conter = right_node_conter + 1;
+                x_l_h = x_l_h + y_ele(aa) * PolyShape(1, right_node_conter, xi_h(qua), 0);
+                dx_dxi_h = dx_dxi_h + y_ele(aa) * PolyShape(1, right_node_conter, xi_h(qua), 1);
+            elseif ismember(IEN(ee,aa),top_h_boundary_nodes)
+                top_node_conter = top_node_conter + 1;
+                x_l_h = x_l_h + x_ele(aa) * PolyShape(1, top_node_conter, xi_h(qua), 0);
+                dx_dxi_h = dx_dxi_h + x_ele(aa) * PolyShape(1, top_node_conter, xi_h(qua), 1);
+            elseif ismember(IEN(ee,aa),bottom_h_boundary_nodes)
+                bottom_node_conter = bottom_node_conter + 1;
+                x_l_h = x_l_h + x_ele(aa) * PolyShape(1, bottom_node_conter, xi_h(qua), 0);
+                dx_dxi_h = dx_dxi_h + x_ele(aa) * PolyShape(1, bottom_node_conter, xi_h(qua), 1);
+            elseif ismember(IEN(ee,aa),circle_h_boundary_nodes)
+                % circle 边界的h为零，所以不做任何操作
+            end
         end
-      end  
+        dxi_dx_h = 1.0 / dx_dxi_h;
+        left_node_conter = 0;
+        right_node_conter = 0;
+        top_node_conter = 0;
+        bottom_node_conter = 0;
+        for aa = 1 : n_en
+            
+            if ismember(IEN(ee,aa),left_h_boundary_nodes)
+                left_node_conter = left_node_conter + 1;
+                h_ele(aa) = h_ele(aa) + weight_h(qua) * PolyShape(1, left_node_conter, xi_h(qua), 0) * h_left(x_l_h) * dx_dxi_h;
+            elseif ismember(IEN(ee,aa),right_h_boundary_nodes)
+                right_node_conter = right_node_conter + 1;
+                h_ele(aa) = h_ele(aa) + weight_h(qua) * PolyShape(1, right_node_conter, xi_h(qua), 0) * h_right(x_l_h) * dx_dxi_h;
+            elseif ismember(IEN(ee,aa),top_h_boundary_nodes)
+                top_node_conter = top_node_conter + 1;
+                h_ele(aa) = h_ele(aa) + weight_h(qua) * PolyShape(1, top_node_conter, xi_h(qua), 0) * h_top(x_l_h) * dx_dxi_h;
+            elseif ismember(IEN(ee,aa),bottom_h_boundary_nodes)
+                bottom_node_conter = bottom_node_conter + 1;
+                h_ele(aa) = h_ele(aa) + weight_h(qua) * PolyShape(1, bottom_node_conter, xi_h(qua), 0) * h_bottom(x_l_h) * dx_dxi_h;
+            elseif ismember(IEN(ee,aa),circle_h_boundary_nodes)
+                % circle 边界的h为零，所以不做任何操作
+            end
+
+        end
     end
-  end
+
+    for aa = 1 : n_en
+        PP = LM(ee, aa);
+        if PP > 0
+            F(PP) = F(PP) + f_ele(aa) - g_ele(aa) + h_ele(aa);
+
+            for bb = 1 : n_en
+                QQ = LM(ee, bb);
+                if QQ > 0
+                    K(PP, QQ) = K(PP, QQ) + k_ele(aa, bb);
+                else
+                    % modify F with the boundary data
+                    % here we do nothing because the boundary data g is zero or
+                    % homogeneous
+                end
+            end
+        end
+
+
+
+    end
 end
 
 % solve the stiffness matrix
@@ -219,13 +302,13 @@ dn = K \ F;
 disp = zeros(n_np, 1);
 
 for ii = 1 : n_np
-  index = ID(ii);
-  if index > 0
-    disp(ii) = dn(index);
-  else
-    % modify disp with the g data. Here it does nothing because g is zero
-    disp(ii) = disp(ii) + gg(ii);
-  end
+    index = ID(ii);
+    if index > 0
+        disp(ii) = dn(index);
+    else
+        % modify disp with the g data. Here it does nothing because g is zero
+        disp(ii) = disp(ii) + gg(ii);
+    end
 end
 
 % save the solution vector and number of elements to disp with name
@@ -238,31 +321,31 @@ save("Solid-quad", "disp", "x_coor", "y_coor");
 % n_sam = 10; %每个单元绘图样本点的个数
 % xi_sam = -1 : (2/n_sam) : 1;
 % yi_sam = -1 : (2/n_sam) : 1;
-% 
+%
 % x_sam = zeros(n_el * n_sam + 1, 1);
 % y_sam = zeros(n_el * n_sam + 1, 1);
 % u_sam = zeros(n_el * n_sam + 1, n_el * n_sam + 1); % store the exact solution value at sampling points
 % uh_sam = zeros(n_el * n_sam + 1, n_el * n_sam + 1); % store the numerical solution value at sampling pts
-% 
+%
 % for ex = 1 : n_el_x
 %     for ey = 1 : n_el_y
 %         ee = (ey-1) * n_el_x + ex;
 %         x_ele = x_coor( IEN(ee, :) );
 %         y_ele = y_coor( IEN(ee, :) );
 %         u_ele = disp( IEN(ee, :) );
-% 
+%
 %         if ex == n_el_x % 最后一个单元多绘制一个点
 %             n_sam_end_x = n_sam+1;
 %         else
 %             n_sam_end_x = n_sam;
 %         end
-% 
+%
 %         if ey == n_el_y % 最后一个单元多绘制一个点
 %             n_sam_end_y = n_sam+1;
 %         else
 %             n_sam_end_y = n_sam;
 %         end
-% 
+%
 %         for ll = 1 : n_sam_end_x
 %             for kk = 1 : n_sam_end_y
 %                 x_l = 0.0;
@@ -273,7 +356,7 @@ save("Solid-quad", "disp", "x_coor", "y_coor");
 %                     y_l = y_l + y_ele(aa) * Quad( aa, xi_sam(ll),yi_sam(kk)); % 局部向全局的坐标变换
 %                     u_l = u_l + u_ele(aa) * Quad( aa, xi_sam(ll),yi_sam(kk)); % u(x)解的表达式
 %                 end
-% 
+%
 %                 x_sam( (ex-1)*n_sam + ll ) = x_l;
 %                 y_sam( (ey-1)*n_sam + kk ) = y_l;
 %                 uh_sam( (ex-1)*n_sam + ll, (ey-1)*n_sam + kk ) = u_l;
